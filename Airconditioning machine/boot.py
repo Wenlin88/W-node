6c97922c68f89c7d0bfab852b86ee64c971292f6
Henri Wenlin <henri.wenlin@kone.com>
2021-12-13 18:30:20 +0200

    Aircodition kooodi lisätty poistoilmapuhaltimen ohjaamiseen

--- Airconditioning machine/boot.py
+++ Airconditioning machine/boot.py
@@ -11,6 +11,10 @@ from ttgo_display import ttgo_display
 gc.collect()
 esp.osdebug(None)
 
+# Initialize TTGO display -->
+display = ttgo_display.display()
+display.tft.rotation(2)
+
 def connect_to_wifi(ssid, pwd):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
@@ -19,21 +23,12 @@ def connect_to_wifi(ssid, pwd):
         sta_if.connect(ssid, pwd)
         while not sta_if.isconnected():
             pass
-    display = ttgo_display.display()
+
+    display.text(sta_if.ifconfig()[0], 0)
     print('Connected!')
-    for i, item in enumerate(sta_if.ifconfig()):
-        display.text(item, i)
+
 
 
 connect_to_wifi(config.SSID, config.wifi_passwd)
-# webrepl.start(password=config.webrepl_passwd)
-
-# f = open('client.key', "r")
-# key_data = f.read()
-# f.close()
-
-# f = open('client.crt', "r")
-# cert_data = f.read()
-# f.close()
-
+webrepl.start(password=config.webrepl_passwd)
 
